93d01ced8687c8e7cb184ca7eefc50cbcf6ed486
Lakshya Agrawal <lakshyaag182005@gmail.com>
2026-01-14 01:17:19 +0530

    modified cors to support hosted demo, TicketSystem v1.0.1

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://ticket-system-khaki.vercel.app"],
     credentials: true,
   })
 );
